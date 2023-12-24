0ddbc6056bbeb742c1ff3b8365a9ec6059a60bfa
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-12-24 15:50:22 +0700

    react-learning

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Color from "../HOC/Color";
+import Background from "../../assets/images/test.png";
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                Hello World from Homepage
-            </div>
+            <>
+                <div>
+                    Hello World from Homepage
+                </div>
+                <div>
+                    <img 
+                        src={ Background }
+                        style={{ width: '200px', height: '200px', marginTop: '20px', borderRadius: '50%' }}
+                    />
+                </div>
+            </>
         )
     }
 }
